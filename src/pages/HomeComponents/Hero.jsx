739240f5af70d21f671bbb1aa1a0dbace37b66ad
sowunmi mayowa <smayowa176@gmail.com>
2024-12-27 05:16:22 +0100

    adjusted hero section

--- src/pages/HomeComponents/Hero.jsx
+++ src/pages/HomeComponents/Hero.jsx
@@ -13,12 +13,12 @@ const Hero = () => {
         }
       ]
   return (
-    <div className="h-3/4 flex justify-center items-center flex-col font-montserrat mx-8 md:mx-12 xl:mx-auto xl:max-w-6xl" >
-        <img src={bgOverlay} alt=""  className="absolute opacity-50 dark:opacity-30"/>
+    <div className="h-3/4 flex justify-end items-baseline md:items-center flex-col font-montserrat mx-8 md:mx-12 xl:mx-auto xl:max-w-6xl" >
+        <img src={bgOverlay} alt=""  className="absolute opacity-50 dark:opacity-50 hidden md:block"/>
         <TypewriterEffectSmooth words={words} />
         <TypewriterEffectSmooth words={words2}  className="hidden md:block"/>
         <h3 className="text-lg font-bold text-center max-w-sm  md:hidden">Welcome to my Portfolio website.</h3>
-        <p className="mb-6 text-md md:text-lg max-w-xl text-center">I transform ideas into digital realities, focusing on creating unique and engaging experiences for users.</p>
+        <p className="mb-6 text-md md:text-lg max-w-xl text-left md:text-center">I transform ideas into digital realities, focusing on creating unique and engaging experiences for users.</p>
         <button className="bg-orange text-white px-6 py-3 rounded-full">Reach out</button>
     </div>
 
