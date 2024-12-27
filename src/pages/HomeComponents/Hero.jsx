51461bc4e203ddd895b2c56e88f6bb69834ebc23
sowunmi mayowa <smayowa176@gmail.com>
2024-12-27 05:18:46 +0100

    adjusted hero section

--- src/pages/HomeComponents/Hero.jsx
+++ src/pages/HomeComponents/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         <img src={bgOverlay} alt=""  className="absolute opacity-50 dark:opacity-50 hidden md:block"/>
         <TypewriterEffectSmooth words={words} />
         <TypewriterEffectSmooth words={words2}  className="hidden md:block"/>
-        <h3 className="text-lg font-bold text-center max-w-sm  md:hidden">Welcome to my Portfolio website.</h3>
+        <h3 className="text-lg font-bold text-left md:text-center max-w-sm  md:hidden">Welcome to my Portfolio website.</h3>
         <p className="mb-6 text-md md:text-lg max-w-xl text-left md:text-center">I transform ideas into digital realities, focusing on creating unique and engaging experiences for users.</p>
         <button className="bg-orange text-white px-6 py-3 rounded-full">Reach out</button>
     </div>
