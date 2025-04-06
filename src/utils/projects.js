9abf51126497797c08ec64cca31e77092e347b68
sowunmi mayowa <smayowa176@gmail.com>
2025-04-06 22:50:23 +0100

    created a file for the projects

---
+++ src/utils/projects.js
@@ -0,0 +1,51 @@
+import projectImg1 from "../assets/project-img1.png"
+import projectImg2 from "../assets/project-img2.png"
+import projectImg3 from "../assets/project-img3.png";
+ 
+ export const projectData = [
+        {
+            id: Math.floor(Math.random() * 1000),
+            title: "SiQuote",
+            company: "Ovabor Labs",
+            stack: ["React", "firebase"],
+            link: "https://si-quotes-v1-1.vercel.app/",
+            image: projectImg1,
+            description: "SiQuote allows users to compare and share pricing for satellite imagery services. It provides a streamlined platform to browse and evaluate quotes for satellite data, simplifying the decision-making process for individuals and organizations requiring geospatial information"
+        },
+        {
+            id: Math.floor(Math.random() * 1000),
+            title: "Equipfy",
+            company: "Ovabor Labs",
+            stack: ["React", "MongoDB", "Tailwind"],
+            link: "https://equipfy-v1-1-1.onrender.com/",
+            image: projectImg2,
+            description: "eQuipfy is a platform for buying and selling used heavy equipment across Africa. It connects buyers and sellers, offering a user-friendly experience to trade machinery efficiently while promoting sustainable use of resources"
+        },
+        {
+            id: Math.floor(Math.random() * 1000),
+            title: "BrightLife Enhancement Service",
+            company: "Client Project",
+            stack: ["React", "MongoDB", "Tailwind"],
+            link: "https://brightlifeenhancementservice.vercel.app/",
+            image: projectImg3,
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus iure porro minima similique suscipit atque quasi distinctio voluptatem fuga, obcaecati inventore expedita persp"
+        },
+        {
+            id: Math.floor(Math.random() * 1000),
+            title: "Equipfy",
+            company: "Ovabor Labs",
+            stack: ["React", "MongoDB", "Tailwind"],
+            link: "https://equipfy-v1-1-1.onrender.com/",
+            image: projectImg2,
+            description: "eQuipfy is a platform for buying and selling used heavy equipment across Africa. It connects buyers and sellers, offering a user-friendly experience to trade machinery efficiently while promoting sustainable use of resources"
+        },
+        {
+            id: Math.floor(Math.random() * 1000),
+            title: "BrightLife Enhancement Service",
+            company: "Client Project",
+            stack: ["React", "MongoDB", "Tailwind"],
+            link: "https://brightlifeenhancementservice.vercel.app/",
+            image: projectImg3,
+            description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus iure porro minima similique suscipit atque quasi distinctio voluptatem fuga, obcaecati inventore expedita persp"
+        }
+    ]
